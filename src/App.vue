59319a3bc06d3399cb43982cb508e42dbd3b2cb7
n3utrino <n3utrino@eleet.ch>
2020-02-17 06:50:39 +0100

    feat: day filter

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,58 @@
 <template>
-  <div id="app" class="appointments">
-    <div class="appointments__day">
-      <span class="appointments__day-header">Optimale Zeit wählen +/- 1h</span>
-      <ul class="appointments__slots">
-        <li
-          v-for="selection in selections"
-          v-bind:key="selection.time"
-          @click="toggleTime(selection)"
-          class="appointments__slot appointments__slot-time-selection"
-          :class="{'appointments__slot-time-selection--active':optimalHours.includes(selection.time)}"
-        >
-          <span >{{selection.time}}:00</span>
-        </li>
-      </ul>
-    </div>
-    <transition-group name="list" tag="ul" class="appointments__day--list">
-      <li class="appointments__day list-item" v-for="day in availableDays" v-bind:key="day.id">
-        <div class="appointments__day-header">
-          <span>{{day.date | relativeFilter}} am&nbsp;</span>
-          <span>{{day.date | dayFilter}}</span>
-        </div>
+  <div id="app">
+    <h1>{{searchDescription}}</h1>
+    <div class="appointments">
+      <div class="appointments__day">
+        <span class="appointments__day-header">Optimalen Tag wählen</span>
         <ul class="appointments__slots">
           <li
-            v-for="slot in day.slots"
-            class="appointments__slot"
-            v-bind:class="{
+            v-for="(selection,index) in dayFilters"
+            v-bind:key="index"
+            @click="toggleDay(index)"
+            class="appointments__slot appointments__slot-time-selection"
+            :class="{'appointments__slot-time-selection--active':optimalDays.includes(index)}"
+          >
+            <span>{{selection}}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="appointments__day">
+        <span class="appointments__day-header">Optimale Zeit wählen <br/>+/- 1h</span>
+        <ul class="appointments__slots">
+          <li
+            v-for="selection in selections"
+            v-bind:key="selection.time"
+            @click="toggleTime(selection)"
+            class="appointments__slot appointments__slot-time-selection"
+            :class="{'appointments__slot-time-selection--active':optimalHours.includes(selection.time)}"
+          >
+            <span>{{selection.time}}:00</span>
+          </li>
+        </ul>
+      </div>
+      <transition-group name="list" tag="ul" class="appointments__day--list">
+        <li class="appointments__day list-item" v-for="day in availableDays" v-bind:key="day.id">
+          <div class="appointments__day-header">
+            <span>{{day.date | relativeFilter}} am&nbsp;</span>
+            <br />
+            <span @click="toggleDay(day.date)">{{day.date | dayFilter}}</span>
+          </div>
+          <ul class="appointments__slots">
+            <li
+              v-for="slot in day.slots"
+              class="appointments__slot"
+              v-bind:class="{
               'appointments__slot--available': slot.available,
               'appointments__slot--optimal':  optimalHours.includes(slot.from.getHours())
               }"
-            v-bind:key="slot.id"
-          >
-            <span>Um {{slot.from | timeFilter}} bis {{slot.to | timeFilter}}</span>
-          </li>
-        </ul>
-      </li>
-    </transition-group>
+              v-bind:key="slot.id"
+            >
+              <span>Um {{slot.from | timeFilter}} bis {{slot.to | timeFilter}}</span>
+            </li>
+          </ul>
+        </li>
+      </transition-group>
+    </div>
   </div>
 </template>
 
@@ -59,6 +77,30 @@ interface Slot {
 let numberOfSlots = 11;
 let slotStart = 8;
 
+const numericHourFormat = new Intl.DateTimeFormat("de-CH", { hour: "numeric" });
+const weekdayFormat = new Intl.DateTimeFormat("de-CH", {
+  weekday: "long"
+});
+
+const dateFormat = new Intl.DateTimeFormat("de-CH", {
+  month: "long",
+  day: "numeric"
+});
+
+let generateDayFilters = () => {
+  let weekdayDate = new Date(Date.UTC(2017, 0, 2)); // Any monday
+  let weekdays = [];
+
+  for (let i = 0; i < 7; i++) {
+    weekdays[weekdayDate.getDay()] = weekdayFormat.format(weekdayDate);
+    weekdayDate.setDate(weekdayDate.getDate() + 1);
+  }
+
+  return weekdays;
+};
+
+let days = generateDayFilters();
+
 let generateSelections = () => {
   let selections = [];
   for (let i = 0; i <= numberOfSlots; i++) {
@@ -69,7 +111,7 @@ let generateSelections = () => {
 
 let generateDays = (): Day[] => {
   let days: Day[] = [];
-  let numberOfDays = 20;
+  let numberOfDays = 60;
 
   let today = new Date();
   today.setHours(slotStart);
@@ -107,16 +149,6 @@ let generateDays = (): Day[] => {
   return days;
 };
 
-const numericHourFormat = new Intl.DateTimeFormat("de-CH", { hour: "numeric" });
-const weekdayFormat = new Intl.DateTimeFormat("de-CH", {
-  weekday: "long"
-});
-
-const dateFormat = new Intl.DateTimeFormat("de-CH", {
-  month: "long",
-  day: "numeric"
-});
-
 const dateFilter = Vue.filter("timeFilter", (date: Date) => {
   return numericHourFormat.format(date);
 });
@@ -153,7 +185,9 @@ const relativeFilter = Vue.filter("relativeFilter", (date: Date) => {
 export default class App extends Vue {
   private days: Day[] = generateDays();
   private optimalHours: number[] = [8, 12, 18];
+  private optimalDays: number[] = [];
 
+  private dayFilters = days;
   private selections = generateSelections();
 
   toggleTime(selection: any) {
@@ -166,11 +200,57 @@ export default class App extends Vue {
     }
   }
 
+  toggleDay(day: number) {
+    let index = this.optimalDays.indexOf(day);
+
+    if (index > -1) {
+      this.optimalDays.splice(index, 1);
+    } else {
+      this.optimalDays.push(day);
+    }
+  }
+
+  get searchDescription(): string {
+    let text = "Den nächsten möglichen Termin finden.";
+
+    if (this.optimalHours.length > 0 || this.optimalDays.length > 0) {
+      text = "Termin ";
+
+      this.optimalHours.forEach((hour, index) => {
+        if (index > 0) {
+          text += " oder um ";
+        } else if (index == 0) {
+          text += " um ";
+        }
+        text += `${hour}:00`;
+
+        if (index == this.optimalHours.length - 1) {
+          text += " Uhr ";
+        }
+      });
+
+      this.optimalDays.forEach((day, index) => {
+        if (index > 0) {
+          text += " oder am ";
+        } else if (index == 0) {
+          text += " am ";
+        }
+        text += days[index];
+      });
+
+      text += " finden";
+    }
+
+    return text;
+  }
+
   get availableDays() {
     return this.days
       .filter((day: Day) => {
         let slots = day.slots.filter(
           slot =>
+            (this.optimalDays.length == 0 ||
+              this.optimalDays.includes(slot.from.getDay())) &&
             (this.optimalHours.length == 0 ||
               this.optimalHours.includes(slot.from.getHours()) ||
               this.optimalHours.includes(slot.from.getHours() + 1) ||
@@ -204,41 +284,49 @@ ul {
 .appointments {
   display: flex;
 
+  $selectionColor: rgb(0, 60, 255);
+
   &__day--list {
     display: flex;
   }
 
   &__day {
     width: 12rem;
-    margin-right: .1rem;
+    margin-right: 0.1rem;
+
+    &-header {
+      min-height: 2.5rem;
+      display: inline-block;
+    }
   }
 
   &__slot {
-    background-color: red;
+    background-color: rgba(255, 0, 0, 0.5);
     padding: 0.5rem;
     &--available {
-      background-color: green;
+      background-color: rgba(0, 128, 0, 0.5);
     }
 
     &--optimal {
-      color: hotpink;
+      color: $selectionColor;
     }
   }
 
   &__slot-time-selection {
     cursor: pointer;
     border-radius: 4px;
-    margin: .25rem;
-    padding: 0.37rem;
+    margin-bottom: 0.25rem;
+    padding: 0.38rem;
     width: 10rem;
+    background-color: rgb(0, 231, 255);
 
     &--active {
-      color: hotpink;
-      border-right: 5px solid hotpink;
+      color: $selectionColor;
+      border-right: 5px solid $selectionColor;
     }
 
     &:hover {
-      color: hotpink;
+      color: $selectionColor;
     }
   }
 }
